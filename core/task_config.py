4797042bc652f4a046bfaa49c5dc78a4a01f3a0e
Arpan Tripathi <42506819+tripathiarpan20@users.noreply.github.com>
2025-01-22 13:29:16 +0000

    Feature/estimates (#220)
    better match synthetic token estimates

--- core/task_config.py
+++ core/task_config.py
@@ -54,7 +54,7 @@ def task_configs_factory() -> dict[str, cmodels.FullTaskConfig]:
                 func="generate_chat_synthetic", kwargs={"model": CHAT_LLAMA_3_2_3B}
             ),
             endpoint=cmodels.Endpoints.chat_completions.value,
-            volume_to_requests_conversion=300,
+            volume_to_requests_conversion=250,
             is_stream=True,
             weight=0.025,
             timeout=2,
@@ -88,7 +88,7 @@ def task_configs_factory() -> dict[str, cmodels.FullTaskConfig]:
                 func="generate_chat_comp_synthetic", kwargs={"model": CHAT_LLAMA_3_2_3B_COMP}
             ),
             endpoint=cmodels.Endpoints.completions.value,
-            volume_to_requests_conversion=300,
+            volume_to_requests_conversion=250,
             is_stream=True,
             weight=0.025,
             timeout=2,
@@ -122,7 +122,7 @@ def task_configs_factory() -> dict[str, cmodels.FullTaskConfig]:
                 func="generate_chat_synthetic", kwargs={"model": CHAT_LLAMA_3_1_70B}
             ),
             endpoint=cmodels.Endpoints.chat_completions.value,
-            volume_to_requests_conversion=300,
+            volume_to_requests_conversion=400,
             is_stream=True,
             weight=0.10,
             timeout=2,
@@ -156,7 +156,7 @@ def task_configs_factory() -> dict[str, cmodels.FullTaskConfig]:
                 func="generate_chat_comp_synthetic", kwargs={"model": CHAT_LLAMA_3_1_70B_COMP}
             ),
             endpoint=cmodels.Endpoints.completions.value,
-            volume_to_requests_conversion=300,
+            volume_to_requests_conversion=400,
             is_stream=True,
             weight=0.10,
             timeout=2,
@@ -256,7 +256,7 @@ def task_configs_factory() -> dict[str, cmodels.FullTaskConfig]:
             ),
             synthetic_generation_config=cmodels.SyntheticGenerationConfig(func="generate_chat_comp_synthetic", kwargs={"model": CHAT_ROGUE_ROSE_103B_COMP}),
             endpoint=cmodels.Endpoints.completions.value,
-            volume_to_requests_conversion=300,
+            volume_to_requests_conversion=600,
             is_stream=True,
             weight=0.10,
             timeout=2,
